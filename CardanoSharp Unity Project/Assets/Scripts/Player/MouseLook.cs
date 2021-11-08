7b07aa54a55cd8dfd62145f1957cd62b748c0926
Javier <jasercam97@gmail.com>
2021-11-08 21:13:53 +0100

    URP - Map Scale - FPPlayer
    -Changed render pipeline to URP
    -Map scale is now x2 to fit player better.
    -Added player controller

---
+++ CardanoSharp Unity Project/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ CardanoSharp Unity Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float gravityForce = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    private CharacterController controller;
+
+    private Vector3 velocity;
+
+    [SerializeField]private bool isGrounded;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0) { velocity.y = -2f; }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * h + transform.forward * v;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) { velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravityForce); }
+
+        velocity.y += gravityForce * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
